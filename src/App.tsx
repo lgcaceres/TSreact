7ff3af44a3ea30bbd1b918acf5113e9323254a8d
Luis <lg.caceres19@gmail.com>
2021-07-01 19:47:57 -0500

    first commmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { count } from 'console';
+
+
+type AppProps = {
+  message:string;
+  count: number;
+}
 
 function App() {
+
+  let vars : AppProps = {
+    message:'Hi luis',
+    count:6
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{vars.message}</p>
     </div>
   );
 }
